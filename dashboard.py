2bb9f86d30e1b6e5070d4b68ff6151db79acc222
sufiXphy <145672679+sufiXphy@users.noreply.github.com>
2023-10-08 19:55:36 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -15,8 +15,8 @@ st.write('Frekuensi plot sinus adalah ',f1)
 st.write('Frekuensi plot cosinus adalah ',f2)
 
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-y = np.sin(x)  # Calculating sin(x) values
-z = np.cos(x)  # Calculating sin(x) values
+y = np.sin(f1*x)  # Calculating sin(x) values
+z = np.cos(f2*x)  # Calculating sin(x) values
  
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
@@ -37,7 +37,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.caption('Sin')
     x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-    y = np.sin(x)  # Calculating sin(x) values
+    y = np.sin(f1*x)  # Calculating sin(x) values
  
     fig, ax = plt.subplots(figsize=(16, 8))
     ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
@@ -52,7 +52,7 @@ with col1:
 with col2:
     st.caption('Cos')
     x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-    y = np.cos(x)  # Calculating sin(x) values
+    y = np.cos(f2*x)  # Calculating sin(x) values
  
     fig, ax = plt.subplots(figsize=(16, 8))
     ax.plot(x, y, label='cos(x)', color='g')  # Plotting cos(x) curve
