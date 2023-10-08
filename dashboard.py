4cd6b686d3857d57f6b2f331de41b29f68178e8a
sufiXphy <145672679+sufiXphy@users.noreply.github.com>
2023-10-08 20:01:21 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,8 +19,8 @@ y = np.sin(f1*x)  # Calculating sin(x) values
 z = np.cos(f2*x)  # Calculating sin(x) values
  
 fig, ax = plt.subplots(figsize=(16, 8))
-ax.plot(x, y, label='sin(x)', color='p')  # Plotting sin(x) curve
-ax.plot(x, z, label='cos(x)', color='c')  # Plotting sin(x) curve
+ax.plot(x, y, label='sin(x)', color='y')  # Plotting sin(x) curve
+ax.plot(x, z, label='cos(x)', color='g')  # Plotting sin(x) curve
 ax.set_ylabel("")
 ax.set_xlabel("x")
 ax.tick_params(axis='y', labelsize=20)
@@ -40,7 +40,7 @@ with col1:
     y = np.sin(f1*x)  # Calculating sin(x) values
  
     fig, ax = plt.subplots(figsize=(16, 8))
-    ax.plot(x, y, label='sin(x)', color='p')  # Plotting sin(x) curve
+    ax.plot(x, y, label='sin(x)', color='y')  # Plotting sin(x) curve
     ax.set_ylabel("Sin x")
     ax.set_xlabel("x")
     ax.tick_params(axis='y', labelsize=20)
@@ -55,7 +55,7 @@ with col2:
     y = np.cos(f2*x)  # Calculating sin(x) values
  
     fig, ax = plt.subplots(figsize=(16, 8))
-    ax.plot(x, y, label='cos(x)', color='c')  # Plotting cos(x) curve
+    ax.plot(x, y, label='cos(x)', color='g')  # Plotting cos(x) curve
     ax.set_ylabel("Cos x")
     ax.set_xlabel("x")
     ax.tick_params(axis='y', labelsize=20)
