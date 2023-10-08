120d9f89498e5211ad94300048966d7fd9bb6661
sufiXphy <145672679+sufiXphy@users.noreply.github.com>
2023-10-08 19:47:40 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,10 @@ import streamlit as st
 # Header
 st.header('Grafik :sparkles:')
 st.subheader('Plot')
- 
+
+nama = st.text_input('Nama', 'Moon', label_visibility='collapsed')
+st.write('Halo ',nama)
+
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
 y = np.sin(x)  # Calculating sin(x) values
 z = np.cos(x)  # Calculating sin(x) values
